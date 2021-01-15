1cd290c204bc22ff69af0d25d2116a1ec25697cd
Igor Savka <igor.savka@phenixrts.com>
2021-01-15 11:41:46 +0200

    Build ChannelViewer2 example (#WSDK-566) Fixed
    Change-Id: I2e8960c3181a8509809703a3556c16e591b4a7fe
    Reviewed-on: http://gerrit.phenixrts.com/c/WebExamples/+/10094
    Reviewed-by: Igor Savka <igor.savka@phenixrts.com>
    Tested-by: Igor Savka <igor.savka@phenixrts.com>
    Reviewed-by: Stefan Birrer <stefan.birrer@phenixrts.com>

---
+++ src/ChannelViewer2/README.md
@@ -0,0 +1,18 @@
+# View Channel
+
+A simple example viewing a channel with `phenixWebsiteDemo` token using the Phenix WebSDK v2 APIs for channels.
+
+## Run
+1. Open our [hosted example](https://phenixrts.com/examples/ChannelViewer2).
+2. (alternatively) Open index.html in a browser.
+3. If you want to test it with your stream, just put your token as a query parameter to the browser url `?token=<your token here>`
+
+## Limitations:
+* Browsers with webRTC support will view the channel in real-time. This includes the most recent versions of most popular browsers (Chrome, Firefox, Edge, Safari, Opera).
+* Legacy browsers will fall back to live streaming (8+ seconds of latency).
+
+## More
+
+* [Browsers' autoplay policies](https://phenixrts.com/docs/faq/index.html#why-isnt-autoplay-working)
+* [Devices in battery saver mode](https://phenixrts.com/docs/faq/index.html#why-is-playback-blocked-in-battery-saver-mode)
+* [Phenix Platform Documentation](http://phenixrts.com/docs/)
---
+++ src/ChannelViewer2/main.js
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2021 Phenix Real Time Solutions, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const phenix = window['phenix'];
+const isMobileAppleDevice = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+let token = 'DIGEST:eyJhcHBsaWNhdGlvbklkIjoiZGVtbyIsImRpZ2VzdCI6IjlDQis2TzNpWUJMWVkydUtaaUJnRjRPaGY1OW9nZkt2R1lwTkc5TlhkeEl3eFRRc0NhalZFMWRQMXRQOWVkaWRCRUdUM2dkdk91WWJiSjVsZ2dWeFF3PT0iLCJ0b2tlbiI6IntcImV4cGlyZXNcIjoxOTI1OTg4ODg0MzkzLFwicmVxdWlyZWRUYWdcIjpcImNoYW5uZWxJZDp1cy1ub3J0aGVhc3QjZGVtbyNwaGVuaXhXZWJzaXRlRGVtb1wifSJ9';
+
+token = new URLSearchParams(location.search).get('token') || token;
+
+const videoElement = document.getElementsByTagName('video')[0];
+const channel = phenix.Channels.createChannel({
+    videoElement,
+    token
+});
+
+document.getElementById('unmuteButton').onclick = () => {
+    document.getElementById('myVideoId').muted = false;
+    document.getElementById('unmuteButton').style.display = 'none';
+    setUserMessage('');
+};
+
+document.getElementById('playButton').onclick = () => {
+    setUserMessage('User triggered play()');
+    channel.play(videoElement);
+    document.getElementById('playButton').style.display = 'none';
+};
+
+function setUserMessage(message) {
+    var statusMessageElement = document.getElementById('userMessage');
+
+    statusMessageElement.innerText = message;
+}
+
+function setStatusMessage(message) {
+    var statusMessageElement = document.getElementById('statusMessage');
+
+    statusMessageElement.innerText = message;
+}
+
+channel.state.subscribe((state) => {
+    setStatusMessage(phenix.ChannelState[state]);
+});
+
+channel.autoMuted.subscribe(autoMuted => {
+    if (autoMuted) {
+        setUserMessage('Video was automatically muted');
+
+        // Show button to unmute
+        document.getElementById('unmuteButton').style.display = '';
+    }
+});
+
+channel.autoPaused.subscribe(autoPaused => {
+    if (autoPaused) {
+        setUserMessage('Video was automatically paused');
+
+        if (isMobileAppleDevice) {
+            // IOS battery saver mode requires user interaction with the <video> to play video
+            videoElement.controls = true;
+            videoElement.onplay = function() {
+                setUserMessage('Video play()');
+                channel.play();
+                videoElement.onplay = null;
+                videoElement.controls = false;
+            };
+        } else {
+            document.getElementById('playButton').style.display = '';
+        }
+    }
+});
