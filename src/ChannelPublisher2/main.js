f8f1b2306e801545c768ab7b348492806c80337a
Igor Savka <igor.savka@phenixrts.com>
2023-07-14 19:35:42 +0300

    Add dropdown menus to select media devices in the ChannelPublisher2 example (#WSDK-777) Fixed
    * Add select for audio source
    * Add select for video source
    * Add disable functionality for audio/video select
    * Move display/enable logic from js to css
    * Change ChannelPublish2 es5 code style to es6
    * Change the state to class logic
    
    Change-Id: I3a53b9bf1222219e9f609fa99d1d9a60c1ad0fd8
    Reviewed-on: https://gerrit.phenixrts.com/c/WebExamples/+/16075
    Reviewed-by: Igor Savka <igor.savka@phenixrts.com>
    Reviewed-by: Nicolas Hebrard <nicolas.hebrard@phenixrts.com>
    Reviewed-by: Alex Zinn <alex.zinn@phenixrts.com>
    Reviewed-by: Jorge-Luis Sanchez-Ponz <jorge.sanchez@phenixrts.com>
    Tested-by: Igor Savka <igor.savka@phenixrts.com>

--- src/ChannelPublisher2/main.js
+++ src/ChannelPublisher2/main.js
@@ -13,84 +13,116 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-var phenix = window['phenix'];
-var videoElement = document.getElementById('myVideoId');
-var publishButton = document.getElementById('publishButton');
-var stopButton = document.getElementById('stopButton');
-var getUserMediaButton = document.getElementById('getUserMediaButton');
-var stopUserMediaButton = document.getElementById('stopUserMediaButton');
-var statusBar = document.getElementById('statusBar');
-var token = new URLSearchParams(location.search).get('token');
-var screenName = new URLSearchParams(location.search).get('screenName') || (Math.random() + 1).toString(36).substring(2);
-var publisher;
-var publisherStateObservable;
-var mediaStream;
+const phenix = window['phenix'];
+const mainContainer = document.getElementById('main');
+const videoElement = document.getElementById('myVideoId');
+const publishButton = document.getElementById('publishButton');
+const stopButton = document.getElementById('stopButton');
+const getUserMediaButton = document.getElementById('getUserMediaButton');
+const stopUserMediaButton = document.getElementById('stopUserMediaButton');
+const controlButtonContainer = document.getElementById('controlButtonContainer');
+const audioDeviceSelect = document.getElementById('audioDeviceSelect');
+const videoDeviceSelect = document.getElementById('videoDeviceSelect');
+const mediaDeviceContainer = document.getElementById('mediaDeviceContainer');
+const mediaDeviceSelectors = [audioDeviceSelect, videoDeviceSelect];
+const token = new URLSearchParams(location.search).get('token');
+const screenName = new URLSearchParams(location.search).get('screenName') || (Math.random() + 1).toString(36).substring(2);
+let publisher;
+let publisherStateObservable;
+let mediaStream;
 
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-}).then(mediaStream_ => {
-    mediaStream = mediaStream_;
-    videoElement.srcObject = mediaStream;
-});
+function retrieveAndPopulateDeviceLists() {
+    return navigator.mediaDevices.enumerateDevices()
+        .then(deviceInfos => {
+            const audioSelectSelectedValue = audioDeviceSelect.value;
+            const videoSelectSelectedValue = videoDeviceSelect.value;
+
+            updateDeviceLists(deviceInfos);
+
+            if (Array.from(audioDeviceSelect.childNodes).some(({value}) => value === audioSelectSelectedValue)) {
+                audioDeviceSelect.value = audioSelectSelectedValue;
+            }
+
+            if (Array.from(videoDeviceSelect.childNodes).some(({value}) => value === videoSelectSelectedValue)) {
+                videoDeviceSelect.value = videoSelectSelectedValue;
+            }
+        });
+}
+
+function updateDeviceLists(deviceInfos) {
+    mediaDeviceSelectors.forEach(mediaDeviceSelector => {
+        Array.from(mediaDeviceSelector.childNodes).forEach(child => {
+            mediaDeviceSelector.removeChild(child);
+        });
+    });
+
+    deviceInfos.forEach(deviceInfo => {
+        const option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+
+        if (deviceInfo.kind === 'audioinput') {
+            option.text = deviceInfo.label || `Microphone ${audioDeviceSelect.length + 1}`;
+            audioDeviceSelect.appendChild(option);
+        } else if (deviceInfo.kind === 'videoinput') {
+            option.text = deviceInfo.label || `Camera ${videoDeviceSelect.length + 1}`;
+            videoDeviceSelect.appendChild(option);
+        } else {
+            console.log(`Some other kind of source/device with label [${deviceInfo.label}], deviceId [${deviceInfo.deviceId}] and kind [${deviceInfo.kind}]`);
+        }
+    });
+
+    const disabledAudioOption = document.createElement('option');
+    const disabledVideoOption = document.createElement('option');
+
+    disabledAudioOption.text = 'No Audio';
+    disabledAudioOption.value = 'none';
+    audioDeviceSelect.appendChild(disabledAudioOption);
+
+    disabledVideoOption.text = 'No Video';
+    disabledVideoOption.value = 'none';
+    videoDeviceSelect.appendChild(disabledVideoOption);
+}
+
+function setMediaStream(stream) {
+    mediaStream = stream;
+    videoElement.srcObject = stream;
+}
+
+function start() {
+    if (mediaStream) {
+        publish();
+
+        return;
+    }
+
+    getUserMedia()
+        .then(publish)
+        .catch(handleError);
+}
 
 function publish() {
-    hideElement(publishButton);
-    hideElement(stopUserMediaButton);
-    displayElement(stopButton);
-    displayElement(statusBar);
-
     publisher = phenix.Publishers.createPublisher({
         mediaStream,
         name: screenName,
         token
     });
 
-    publisherStateObservable = publisher.state.subscribe(function(state) {
+    publisherStateObservable = publisher.state.subscribe(state => {
         setStatusMessage(phenix.PublisherState[state]);
-
-        switch (state) {
-        case phenix.PublisherState.Publishing:
-            videoElement.className = 'publisher-online';
-
-            break;
-        case phenix.PublisherState.Starting:
-            videoElement.className = 'publisher-starting';
-
-            break;
-        case phenix.PublisherState.Recovering:
-        case phenix.PublisherState.Reconnecting:
-            videoElement.className = 'publisher-recovering';
-
-            break;
-        case phenix.PublisherState.Unauthorized:
-        case phenix.PublisherState.GeoRestricted:
-        case phenix.PublisherState.GeoBlocked:
-        case phenix.PublisherState.UnsupportedFeature:
-        case phenix.PublisherState.NotFound:
-        case phenix.PublisherState.Error:
-            videoElement.className = 'publisher-offline';
-
-            break;
-        default:
-            videoElement.className = '';
-        }
+        setStatusClass(phenix.PublisherState[state] || '');
     });
 }
 
 // Clean up publisher
 function stopPublisher() {
     if (publisher) {
-        publisher.stop().then(() => {
-            publisher = null;
-            publisherStateObservable.dispose();
-            publisherStateObservable = null;
-        });
+        publisher.stop()
+            .then(() => {
+                publisher = null;
+                publisherStateObservable.dispose();
+                publisherStateObservable = null;
+            });
     }
-
-    hideElement(stopButton);
-    displayElement(publishButton);
-    displayElement(stopUserMediaButton);
 }
 
 function stopUserMedia() {
@@ -98,42 +130,68 @@ function stopUserMedia() {
         .forEach(track => track.stop());
     mediaStream = null;
     videoElement.srcObject = null;
-
-    hideElement(publishButton);
-    hideElement(stopUserMediaButton);
-    displayElement(getUserMediaButton);
+    mediaDeviceContainer.className = 'stopped-user-media';
+    controlButtonContainer.className = 'stopped-user-media';
 }
 
 function getUserMedia() {
-    navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true
-    }).then(mediaStream_ => {
-        mediaStream = mediaStream_;
-        videoElement.srcObject = mediaStream;
+    if (mediaStream) {
+        mediaStream.getTracks()
+            .forEach(track => track.stop());
+        mediaStream = null;
+    }
 
-        hideElement(getUserMediaButton);
-        displayElement(publishButton);
-    });
+    const audioSource = audioDeviceSelect.value;
+    const audioSourceDisabled = audioDeviceSelect.value === 'none';
+    const videoSource = videoDeviceSelect.value;
+    const videoSourceDisabled = videoDeviceSelect.value === 'none';
+
+    if (audioSourceDisabled && videoSourceDisabled) {
+        console.error('No media is available');
+
+        return;
+    }
+
+    const constrains = {
+        audio: audioSourceDisabled ? false : {deviceId: audioSource ? {exact: audioSource} : undefined},
+        video: videoSourceDisabled ? false : {deviceId: videoSource ? {exact: videoSource} : undefined}
+    };
+
+    return navigator.mediaDevices.getUserMedia(constrains)
+        .then(setMediaStream)
+        .then(() => {
+            mediaDeviceContainer.className = '';
+            controlButtonContainer.className = '';
+        })
+        .then(retrieveAndPopulateDeviceLists);
 }
 
 function setStatusMessage(message) {
-    var statusMessageElement = document.getElementById('statusMessage');
+    const statusMessageElement = document.getElementById('statusMessage');
 
     statusMessageElement.innerText = message;
 }
 
-function displayElement(element) {
-    element.className = element.className.substring(0, element.className.indexOf(' hide'));
+function setStatusClass(status) {
+    const camelCaseToKebabCase = value => value
+        .replace(/([A-Z])/g, (match, group) => `-${group.toLowerCase()}`)
+        .replace(/^-/, '');
+
+    mainContainer.className = `publisher-${camelCaseToKebabCase(status)}`;
 }
 
-function hideElement(element) {
-    if (element.className.indexOf('hide') === -1) {
-        element.className += ' hide';
-    }
+function handleError(error) {
+    console.log(`navigator.MediaDevices.getUserMedia error [${error.name}] with message [${error.message}]`);
 }
 
-publishButton.onclick = publish;
+retrieveAndPopulateDeviceLists()
+    .catch(handleError);
+
+start();
+
+audioDeviceSelect.onchange = getUserMedia;
+videoDeviceSelect.onchange = getUserMedia;
+publishButton.onclick = start;
 stopButton.onclick = stopPublisher;
 getUserMediaButton.onclick = getUserMedia;
 stopUserMediaButton.onclick = stopUserMedia;
